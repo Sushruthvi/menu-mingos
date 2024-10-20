59d17d03f6ffc297ba375404988767e82c5bafa1
Sushruthvi <137644925+Sushruthvi@users.noreply.github.com>
2024-10-20 22:03:07 +0530

    added price

--- src/components/MenuCard.js
+++ src/components/MenuCard.js
@@ -9,21 +9,24 @@ const MenuCard = ({menuData}) => {
     const{id,image,name,category,price,description} = curElem;
     return(
         <>
-         <div className='card-container'key={id}>
-         <div className='card'>
-           <div className='card-body'>
-             <span className='card-number card-circle subtle'>{id}</span>
-             <span className='card-author'>{category}</span>
-             <h2 className='card-title'>{name}</h2>
-             <span className='card-description subtle'>
-               {description}
-             </span>
-             <div className='card-read'>Read</div>
-             <img src={image}  alt='{name}' className='card-media' />
-             <span className='card-tag subtle'>Order Now</span>
-           </div>
-         </div>
-       </div>
+       <div className='card-container' key={id}>
+  <div className='card'>
+    <div className='card-body'>
+      <span className='card-number card-circle subtle'>{id}</span>
+      <span className='card-author'>{category}</span>
+      <h2 className='card-title'>{name}</h2>
+      <span className='card-description subtle'>
+        {description}
+      </span>
+      
+      <div className='card-read'>Read</div>
+      <img src={image} alt={name} className='card-media' />
+      <div className='card-price subtle'>Price:{price}</div> 
+      <span className='card-tag subtle'>Order Now</span>
+    </div>
+  </div>
+</div>
+
        
        </>
     )
