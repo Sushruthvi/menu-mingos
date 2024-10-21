c7a50250898f717f5a82b7a591c566011b2e0cb2
ArjunM2005 <135345989+ArjunM2005@users.noreply.github.com>
2024-10-21 23:16:46 +0530

    added images and changed json address

--- src/components/MenuApi.js
+++ src/components/MenuApi.js
@@ -4,42 +4,42 @@ const Menu = [
     "item_name": "Chicken Burger",
     "category": "nonveg",
     "price": 70.00,
-    "image_url": ""
+    "image_url": "/images/chickenburger.jpg"
   },
   {
     "item_id": 2,
     "item_name": "Chicken Sandwich",
     "category": "nonveg",
     "price": 80.00,
-    "image_url": ""
+    "image_url": "/images/chickensandwich.jpeg"
   }, 
   {
     "item_id": 3,
     "item_name": "Chicken Roll",
     "category": "nonveg",
     "price": 90.00,
-    "image_url": ""
+    "image_url": "/images/chickenroll.jpg"
   },
   {
     "item_id": 4,
     "item_name": "Chicken Biriyani",
     "category": "nonveg",
     "price": 130.00,
-    "image_url": ""
+    "image_url": "/images/chickenbiryani.jpg"
   },
   {
     "item_id": 5,
     "item_name": "Chicken Fried Rice",
     "category": "nonveg",
     "price": 100.00,
-    "image_url": ""
+    "image_url": "/images/chickenfriedrice.webp"
   },
   {
     "item_id": 6,
     "item_name": "Chicken Noodles",
     "category": "nonveg",
     "price": 100.00,
-    "image_url": ""
+    "image_url": "/images/chickennoodles.jpg"
   },
 
 
@@ -48,35 +48,35 @@ const Menu = [
     "item_name": "Egg Roll",
     "category": "egg",
     "price": 60.00,
-    "image_url": ""
+    "image_url": "/images/eggroll.jpg"
   },
   {
     "item_id": 8,
     "item_name": "Egg Omelette",
     "category": "egg",
     "price": 40.00,
-    "image_url": ""
+    "image_url": "/images/eggomelette.jpg"
   },
   {
     "item_id": 9,
     "item_name": "Boiled Egg",
     "category": "egg",
     "price": 25.00,
-    "image_url": ""
+    "image_url": "/images/boiledegg.jpeg"
   },
   {
     "item_id": 10,
     "item_name": "Egg Fried Rice",
     "category": "egg",
     "price": 80.00,
-    "image_url": ""
+    "image_url": "/images/eggfriedrice.jpeg"
   },
   {
     "item_id": 11,
     "item_name": "Egg Biryani",
     "category": "egg",
     "price": 110.00,
-    "image_url": ""
+    "image_url": "/images/eggbiryani.jpeg"
   },
 
 
@@ -85,174 +85,174 @@ const Menu = [
     "item_name": "Chole Batura",
     "category": "NorthIndian",
     "price": 80.00,
-    "image_url": ""
+    "image_url": "/images/cholebhature.jpeg"
   },
   {
     "item_id": 13,
     "item_name": "Dal Khichadi",
     "category": "NorthIndian",
     "price": 60.00,
-    "image_url": ""
-  },
-  {
-    "item_id": 13,
-    "item_name": "Aloo Paratha",
-    "category": "NorthIndian",
-    "price": 60.00,
-    "image_url": ""
+    "image_url": "/images/dalkhichdi.webp"
   },
   {
     "item_id": 14,
-    "item_name": "Jeera Rice with Dal Tadka",
+    "item_name": "Aloo Paratha",
     "category": "NorthIndian",
     "price": 60.00,
-    "image_url": ""
+    "image_url": "/images/alooparatha.webp"
+  },
+  {
+    "item_id": 15,
+    "item_name": "Jeera Rice",
+    "category": "NorthIndian",
+    "price": 60.00,
+    "image_url": "/images/jeerarice.jpg"
   },
 
 
   {
-    "item_id": 15,
+    "item_id": 16,
     "item_name": "Idly",
     "category": "SouthIndian",
     "price": 30.00,
-    "image_url": ""
-  },
-  {
-    "item_id": 16,
-    "item_name": "Vada",
-    "category": "SouthIndian",
-    "price": 25.00,
-    "image_url": ""
+    "image_url": "/images/idli.jpg"
   },
   {
     "item_id": 17,
-    "item_name": "Rice Bath",
+    "item_name": "Vada",
     "category": "SouthIndian",
-    "price": 40.00,
-    "image_url": ""
+    "price": 25.00,
+    "image_url": "/images/vada.webp"
   },
   {
     "item_id": 18,
+    "item_name": "Rice Bath",
+    "category": "SouthIndian",
+    "price": 40.00,
+    "image_url": "/images/ricebath.jpeg"
+  },
+  {
+    "item_id": 19,
     "item_name": "Poori Sagu",
     "category": "SouthIndian",
     "price": 60.00,
-    "image_url": ""
+    "image_url": "/images/poorisagu.jpeg"
   },
 
   {
-    "item_id": 19,
+    "item_id": 20,
     "item_name": "Pani Puri",
     "category": "chats",
     "price": 30.00,
-    "image_url": ""
-  },
-  {
-    "item_id": 20,
-    "item_name": "Masala Puri",
-    "category": "chats",
-    "price": 40.00,
-    "image_url": ""
+    "image_url": "/images/panipuri.webp"
   },
   {
     "item_id": 21,
-    "item_name": "Bhel Puri",
+    "item_name": "Masala Puri",
     "category": "chats",
     "price": 40.00,
-    "image_url": ""
+    "image_url": "/images/masalapuri.jpeg"
   },
   {
     "item_id": 22,
-    "item_name": "Dahi Puri",
+    "item_name": "Bhel Puri",
     "category": "chats",
-    "price": 50.00,
-    "image_url": ""
+    "price": 40.00,
+    "image_url": "/images/bhelpuri.jpg"
   },
   {
     "item_id": 23,
-    "item_name": "Pav Bhaji",
+    "item_name": "Dahi Puri",
     "category": "chats",
-    "price": 60.00,
-    "image_url": ""
+    "price": 50.00,
+    "image_url": "/images/dahipuri.jpg"
   },
   {
     "item_id": 24,
+    "item_name": "Pav Bhaji",
+    "category": "chats",
+    "price": 60.00,
+    "image_url": "/images/pavbhaji.jpeg"
+  },
+  {
+    "item_id": 25,
     "item_name": "Sev Puri",
     "category": "chats",
     "price": 40.00,
-    "image_url": ""
+    "image_url": "/images/sevpuri.jpg"
   },
 
   {
-    "item_id": 25,
+    "item_id": 26,
     "item_name": "Plain Dosa",
     "category": "dosa",
     "price": 40.00,
-    "image_url": ""
-  },
-  {
-    "item_id": 26,
-    "item_name": "Masala Dosa",
-    "category": "dosa",
-    "price": 50.00,
-    "image_url": ""
+    "image_url": "/images/plaindosa.jpg"
   },
   {
     "item_id": 27,
-    "item_name": "Set Dosa",
+    "item_name": "Masala Dosa",
     "category": "dosa",
     "price": 50.00,
-    "image_url": ""
+    "image_url": "/images/masaladosa.webp"
   },
   {
     "item_id": 28,
-    "item_name": "Onion Dosa",
+    "item_name": "Set Dosa",
     "category": "dosa",
-    "price": 80.00,
-    "image_url": ""
+    "price": 50.00,
+    "image_url": "/images/setdosa.webp"
   },
   {
     "item_id": 29,
+    "item_name": "Onion Dosa",
+    "category": "dosa",
+    "price": 80.00,
+    "image_url": "/images/oniondosa.webp"
+  },
+  {
+    "item_id": 30,
     "item_name": "Ghee Podi Dosa",
     "category": "dosa",
     "price": 70.00,
-    "image_url": ""
+    "image_url": "/images/gheepodidosa.jpg"
   },
 
 
   {
-    "item_id": 30,
+    "item_id": 31,
     "item_name": " Lemon Ice Tea",
     "category": "ColdBeverages",
     "price": 30.00,
-    "image_url": ""
-  },
-  {
-    "item_id": 31,
-    "item_name": "Cold Coffee",
-    "category": "ColdBeverages",
-    "price": 40.00,
-    "image_url": ""
+    "image_url": "/images/lemonicete.jpeg"
   },
   {
     "item_id": 32,
-    "item_name": "Buttermilk",
+    "item_name": "Cold Coffee",
     "category": "ColdBeverages",
-    "price": 20.00,
-    "image_url": ""
+    "price": 40.00,
+    "image_url": "/images/coldcoffee.webp"
   },
   {
     "item_id": 33,
-    "item_name": "Cold Drink",
+    "item_name": "Buttermilk",
     "category": "ColdBeverages",
     "price": 20.00,
-    "image_url": ""
+    "image_url": "/images/buttermilk.webp"
   },
   {
     "item_id": 34,
+    "item_name": "Cold Drink",
+    "category": "ColdBeverages",
+    "price": 20.00,
+    "image_url": "/images/colddrink.webp"
+  },
+  {
+    "item_id": 35,
     "item_name": "Fresh Juice",
     "category": "ColdBeverages",
     "price": 40.00,
-    "image_url": ""
+    "image_url": "/images/freshjuice.webp"
   }
 ];
 
