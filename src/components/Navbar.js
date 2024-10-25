1cfd25f31da370244448cfe2f7b9ac0e4d660b41
Sushruthvi <137644925+Sushruthvi@users.noreply.github.com>
2024-10-25 20:15:29 +0530

    added cart icon and linked it to orders page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,32 @@
-import React from 'react'
-import MenuCard from './MenuCard'
-const Navbar = ({filterItem, menuList }) => {
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { MdBorderColor } from "react-icons/md";
+
+const Navbar = ({ filterItem, menuList }) => {
   return (
     <nav className='navbar'>
-    <div className='btn-group'>
-        {
-            menuList.map((curElem)=>{
-                return(
-                    <button className='btn-group__item' 
-                    onClick={()=>filterItem(curElem)}>
-                        {curElem}</button>
-                )
-            })
-        }
-   
-    </div>
-  </nav>
-  )
-}
+      <div className='btn-group'>
+        {/* Map through the menuList to render each button */}
+        {menuList.map((curElem) => {
+          return (
+            <button
+              className='btn-group__item'
+              onClick={() => filterItem(curElem)}
+              key={curElem} // Add key for unique identifier
+            >
+              {curElem}
+             
+            </button>
+            
+          );
+        })}
+         <NavLink to="/orders" className='link-to-orders'>
+         <MdBorderColor/> Orders
+        </NavLink>
+       
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
