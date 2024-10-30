cabf203fb92742cf62c52066b4844a312234c01b
Sushruthvi <137644925+Sushruthvi@users.noreply.github.com>
2024-10-30 14:47:27 +0530

    Cart is connected to localStorage

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -1,11 +1,19 @@
-import { createContext,useReducer,useContext } from "react";
+import { createContext,useReducer,useContext, useEffect } from "react";
 import reducer from "../reducer/cartReducer";
 
 
 const CartContext = createContext();
-
+const getLocalCartData = () => {
+    let localCartData=localStorage.getItem("mycart");
+    if(localCartData===null){
+        return [];
+    }
+    else{
+        return JSON.parse(localCartData);
+    }
+}
 const initialState = {
-    cart: [],
+    cart: getLocalCartData(),
     total_item:" ",
     total_amount: 0,
 }
@@ -17,7 +25,9 @@ const addToCart=(item_id, amount, curElem)=>{
 const removeItem=(id)=>{
     dispatch({type:"REMOVE_ITEM", payload:id})
 }
-
+useEffect(()=>{
+    localStorage.setItem("mycart", JSON.stringify(state.cart))
+},[state.cart])
 
     return (<CartContext.Provider value={{ ...state, addToCart, removeItem}}>{children}</CartContext.Provider>);
 }
