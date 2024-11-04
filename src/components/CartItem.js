e384042ad5b6c870b93170e96433859bb7834b4e
Sushruthvi <137644925+Sushruthvi@users.noreply.github.com>
2024-11-04 12:17:44 +0530

    Connected with mongoDB and changed the css of orders page

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,47 +1,40 @@
-import React from 'react'
-import CartAmountToggle from './CartAmountToggle'
+import React from 'react';
+import CartAmountToggle from './CartAmountToggle';
 import { FaTrash } from "react-icons/fa";
 import { useCartContext } from '../context/cart_context';
 
-const CartItem = ({id,name,category,price,amount}) => {
-  const {removeItem}=useCartContext()
-    const  setDecrease = () => {
-        // amount > 1 ? setAmount(amount - 1) : setAmount(1);
-      };
-    
-      const setIncrease = () => {
-        // amount < stock ? setAmount(amount + 1) : setAmount(stock);
-      };
-  return <div className='cart_heading grid grid-five-column'>
-   <div className="cart-image--name">
-        <div>
-          <figure>
-            {name}
-          </figure>
-    </div>
-    <div className="cart-hide">
-       <p>{price}</p>
-    </div>
-    <CartAmountToggle
-        amount={amount}
-        setDecrease={setDecrease}
-        setIncrease={setIncrease}
-      />
-        </div>
-
-        <div className="cart-hide">
-            <p>{price*amount}</p>
-        </div>
-
-        <div>
-            <FaTrash className='remove-icon'  cursor="pointer" onClick={() => {removeItem(id)}}/>
-
-        
-        </div>
-
-        </div>
+const CartItem = ({ id, name, category, price, amount }) => {
+  const { removeItem } = useCartContext();
 
  
-}
+  return (
+    <div className="cart-item-row grid grid-five-column">
+      {/* Item Name */}
+      <div className="cart-image--name">
+        <figure>{name}</figure>
+      </div>
 
-export default CartItem
+      {/* Price */}
+      <div className="cart-hide">
+        <p>${price}</p>
+      </div>
+
+      {/* Quantity Adjustment */}
+      <div>
+         amount={amount} 
+      </div>
+
+      {/* Subtotal */}
+      <div className="cart-hide">
+        <p>${price * amount}</p>
+      </div>
+
+      {/* Remove Button */}
+      <div>
+        <FaTrash className="remove-icon" cursor="pointer" onClick={() => removeItem(id)} />
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -1,38 +1,59 @@
-import { createContext,useReducer,useContext, useEffect } from "react";
+import { createContext, useReducer, useContext, useEffect } from "react";
+import axios from "axios";
 import reducer from "../reducer/cartReducer";
 
-
 const CartContext = createContext();
-const getLocalCartData = () => {
-    let localCartData=localStorage.getItem("mycart");
-    if(localCartData===null){
-        return [];
-    }
-    else{
-        return JSON.parse(localCartData);
-    }
-}
 const initialState = {
-    cart: getLocalCartData(),
-    total_item:" ",
+    cart: [],
+    total_item: " ",
     total_amount: 0,
-}
+};
+
 const CartProvider = ({ children }) => {
     const [state, dispatch] = useReducer(reducer, initialState);
-const addToCart=(item_id, amount, curElem)=>{
-        dispatch({type:"ADD_TO_CART", payload:{item_id, amount, curElem}})
-    }
-const removeItem=(id)=>{
-    dispatch({type:"REMOVE_ITEM", payload:id})
-}
-useEffect(()=>{
-    dispatch({type:"TOTAL_AMOUNT"})
-    localStorage.setItem("mycart", JSON.stringify(state.cart))
-},[state.cart])
 
-    return (<CartContext.Provider value={{ ...state, addToCart, removeItem}}>{children}</CartContext.Provider>);
-}
+    // Load cart data from MongoDB
+    const fetchCartData = async () => {
+        const response = await axios.get("http://localhost:5000/cart");
+        dispatch({ type: "LOAD_CART", payload: response.data });
+    };
+
+    // Add item to cart in MongoDB
+    const addToCart = async (item_id, amount, curElem) => {
+        const cartItem = {
+            id: item_id,
+            name: curElem.item_name,
+            amount: amount,
+            price: curElem.price,
+            category: curElem.category,
+        };
+        await axios.post("http://localhost:5000/cart", cartItem);
+        dispatch({ type: "ADD_TO_CART", payload: cartItem });
+    };
+
+    // Remove item from cart in MongoDB
+    const removeItem = async (id) => {
+        await axios.delete(`http://localhost:5000/cart/${id}`);
+        dispatch({ type: "REMOVE_ITEM", payload: id });
+    };
+
+    useEffect(() => {
+        fetchCartData();
+    }, []);
+
+    useEffect(() => {
+        dispatch({ type: "TOTAL_AMOUNT" });
+    }, [state.cart]);
+
+    return (
+        <CartContext.Provider value={{ ...state, addToCart, removeItem }}>
+            {children}
+        </CartContext.Provider>
+    );
+};
+
 const useCartContext = () => {
     return useContext(CartContext);
-}
-export{CartProvider, useCartContext}
+};
+
+export { CartProvider, useCartContext };
