e0bb36abccfb2d9e45627ce48ae555548627f015
Sushruthvi <137644925+Sushruthvi@users.noreply.github.com>
2024-11-07 16:26:46 +0530

    c

--- src/components/Login.js
+++ src/components/Login.js
@@ -12,7 +12,7 @@ function Login(){
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    axios.post('http://localhost:3001/login', {userid,password})
+    axios.post('http://localhost:5000/login', {userid,password})
     .then(result => {
       console.log(result)
       if(result.data === "Login Successful") {
